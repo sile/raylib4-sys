bc55d59f92dc5cc68759b0921e90b14bf131b594
Takeru Ohta <phjgt308@gmail.com>
2022-01-01 19:48:14 +0900

    Add a workaround for docs.rs build

--- build.rs
+++ build.rs
@@ -7,14 +7,18 @@ fn main() {
     println!("cargo:rustc-link-lib=raylib");
     println!("cargo:rerun-if-changed=wrapper.h");
 
-    let bindings = bindgen::Builder::default()
-        .header("wrapper.h")
-        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
-        .generate()
-        .expect("Unable to generate bindings");
-
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    bindings
-        .write_to_file(out_path.join("bindings.rs"))
-        .expect("Couldn't write bindings!");
+    if cfg!(feature = "docs-rs") {
+        std::fs::copy("bindings-for-docs-rs.rs", out_path.join("bindings.rs"))
+            .expect("Couldn't write bindings!");
+    } else {
+        let bindings = bindgen::Builder::default()
+            .header("wrapper.h")
+            .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+            .generate()
+            .expect("Unable to generate bindings");
+        bindings
+            .write_to_file(out_path.join("bindings.rs"))
+            .expect("Couldn't write bindings!");
+    }
 }
